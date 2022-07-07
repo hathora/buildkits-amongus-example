2ed7345b44cec2d2b563a10f8a99c5d4b757996d
Harsh Pandey <hpandey793@gmail.com>
2022-07-07 18:30:28 -0400

    renames

--- server/server.ts
+++ server/server.ts
@@ -1,49 +1,48 @@
 import { register } from "./protocol.js";
 
-type StateId = bigint;
+type RoomId = bigint;
 type UserId = string;
 type Point = { x: number; y: number };
 type GameState = Map<UserId, { current: Point; target: Point }>;
-const states: Map<StateId, { subscribers: Set<UserId>; game: GameState }> = new Map();
+const states: Map<RoomId, { subscribers: Set<UserId>; game: GameState }> = new Map();
 
 const coordinator = await register({
-  newState(stateId, userId, data) {
-    console.log("newState", stateId.toString(36), userId, data);
-    states.set(stateId, { subscribers: new Set(), game: new Map() });
+  newState(roomId, userId, data) {
+    console.log("newState", roomId.toString(36), userId, data);
+    states.set(roomId, { subscribers: new Set(), game: new Map() });
   },
-  subscribeUser(stateId, userId) {
-    console.log("subscribeUser", stateId.toString(36), userId);
-    const { subscribers, game } = states.get(stateId)!;
+  subscribeUser(roomId, userId) {
+    console.log("subscribeUser", roomId.toString(36), userId);
+    const { subscribers, game } = states.get(roomId)!;
     subscribers.add(userId);
     if (!game.has(userId)) {
       game.set(userId, { current: { x: 4900, y: 1700 }, target: { x: 4900, y: 1700 } });
     }
   },
-  unsubscribeUser(stateId, userId) {
-    console.log("unsubscribeUser", stateId.toString(36), userId);
-    const { subscribers, game } = states.get(stateId)!;
-    subscribers.delete(userId);
+  unsubscribeUser(roomId, userId) {
+    console.log("unsubscribeUser", roomId.toString(36), userId);
+    states.get(roomId)!.subscribers.delete(userId);
   },
   unsubscribeAll() {
     console.log("unsubscribeAll");
   },
-  handleUpdate(stateId, userId, data) {
+  handleUpdate(roomId, userId, data) {
     const dataStr = Buffer.from(data.buffer, data.byteOffset, data.byteLength).toString("utf8");
-    console.log("handleUpdate", stateId.toString(36), userId, dataStr);
-    states.get(stateId)!.game.get(userId)!.target = JSON.parse(dataStr);
+    console.log("handleUpdate", roomId.toString(36), userId, dataStr);
+    states.get(roomId)!.game.get(userId)!.target = JSON.parse(dataStr);
   },
 });
 
-function broadcastUpdates(stateId: bigint) {
-  const { subscribers, game } = states.get(stateId)!;
+function broadcastUpdates(roomId: RoomId) {
+  const { subscribers, game } = states.get(roomId)!;
   const data = [...game.entries()].map(([userId, { current }]) => ({ id: userId, x: current.x, y: current.y }));
   subscribers.forEach((userId) => {
-    coordinator.stateUpdate(stateId, userId, Buffer.from(JSON.stringify(data), "utf8"));
+    coordinator.stateUpdate(roomId, userId, Buffer.from(JSON.stringify(data), "utf8"));
   });
 }
 
 setInterval(() => {
-  states.forEach(({ game, subscribers }, stateId) => {
+  states.forEach(({ game, subscribers }, roomId) => {
     subscribers.forEach((userId) => {
       const position = game.get(userId)!;
       const { current, target } = position;
@@ -61,6 +60,6 @@ setInterval(() => {
         current.y += (dy / dist) * pixelsToMove;
       }
     });
-    broadcastUpdates(stateId);
+    broadcastUpdates(roomId);
   });
 }, 50);
